fe03ba07f988f6e5889ea8efe17fc8e42126ead6
Senad Jukic <10435409+senadjukic@users.noreply.github.com>
2022-09-09 23:08:54 +0200

    changed openweather key to optional

--- aws-lambda-sink-connector-invocation/python/index.py
+++ aws-lambda-sink-connector-invocation/python/index.py
@@ -25,25 +25,27 @@ def lambda_handler(event, context):
     # Show input from event
     print("Event")
     print(event)
-    
-    # Go for real temperature of your city: https://www.latlong.net/
-    api_key = env_openweather_key
-
-    # Example: Zurich
-    lat = "47.376888"
-    lon = "8.541694"
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
-    response = requests.get(url)
-    data = json.loads(response.text)
-    zurich_real_temperature = data["current"]["temp"]
-
-    print("Temperature in Zurich from the OpenWeather API:")
-    print(zurich_real_temperature)
 
     print("Guessed Temperature Value:")
     record = event[0]['payload']['value'].replace('{temperature_guess=', '').replace('}', '')
     print(record)
+    
+    # Go for real temperature of your city: https://www.latlong.net
+    # Example: Zurich
+    if env_openweather_key:
+        lat = "47.376888"
+        lon = "8.541694"
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, env_openweather_key)
+        response = requests.get(url)
+        data = json.loads(response.text)
+        zurich_real_temperature = data["current"]["temp"]
 
-    print("Difference from Zurich's real temperature to guessed temperature:")
-    diff = math.fabs(zurich_real_temperature - float(record))
-    print(diff)
+        print("Temperature in Zurich from the OpenWeather API:")
+        print(zurich_real_temperature)
+
+        print("Difference from Zurich's real temperature to guessed temperature:")
+        diff = math.fabs(zurich_real_temperature - float(record))
+        print(diff)
+
+    else:
+        print("No Openweather key found to calculate difference between guessed and real temperature.")
